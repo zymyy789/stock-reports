79a3fcdb15f14c2d3501930f0e0bf8af5b01824f
zymyy789 <55432174+zymyy789@users.noreply.github.com>
2026-04-08 18:58:59 +0800

    Add Flutter Web app (2026-04-08 18:58)

---
+++ web/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'stock-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
+});
